7dc1a79a54abef2035a6d0efb29de258318a85b4
michalchrapczynski <michal.chrapczynski@gmail.com>
2015-10-29 07:45:19 +0100

    usucięcie geterów i setterów z Taxi, poprawa funkciolanlności Consumer, usunięcie interfajsu

--- src/com/capgemini/taxi/Consumer.java
+++ src/com/capgemini/taxi/Consumer.java
@@ -4,36 +4,36 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-public class Consumer implements Observer {
-	List<Taxi> taxis = new ArrayList<>();
-	Taxi nearestTaxi = null;
-	int valueX = 0;
-	int valueY = 0;
-	Random r = new Random();
+public class Consumer {
+	private List<Taxi> taxis = new ArrayList<>();
+	private Random rand = new Random();
 
 	public Consumer() {
+		// empty
+	}
+
+	/**
+	 * for unit testing
+	 */
+	public Consumer(Random rand) {
+		this.rand = rand;
 	}
 
 	public void run(int x, int y) {
-		valueX = x;
-		valueY = y;
-		createListTaxis();
-		int timeCatchTaxi = r.nextInt(300) + 5;
-		for (int i = 0; i < timeCatchTaxi; i++) {
-			updateListTaxis();
-			/*
-			 * for (Taxi taxi : taxis) { System.out.println(taxi);
-			 * System.out.println(); }
-			 */
-			removeTaxisFromList();
-			addTaxisToList();
-			checkNearestTaxi();
+		createTaxis(x, y);
+		int timeCatchTaxi = rand.nextInt(300) + 5;
 
-			int checkDistanceX = Math.abs(nearestTaxi.getX() - valueX);
-			int checkDistanceY = Math.abs(nearestTaxi.getY() - valueY);
+		for (int i = 0; i < timeCatchTaxi; i++) {
+			updateTaxis();
+			removeTaxis();
+			addTaxis(x, y);
 
 			if (i == timeCatchTaxi - 1) {
-				if (checkDistanceX < 1000 && checkDistanceY < 1000) {
+				Taxi nearestTaxi = findNearestTaxi(x, y);
+				int distanceX = Math.abs(nearestTaxi.x - x);
+				int distanceY = Math.abs(nearestTaxi.y - y);
+
+				if (distanceX < 1000 && distanceY < 1000) {
 					System.out.println("My position is : x=" + x + " y=" + y);
 					System.out.println("I waited for taxi : " + timeCatchTaxi + "s");
 					System.out.println("Nearest taxi position is : " + nearestTaxi);
@@ -42,93 +42,84 @@ public class Consumer implements Observer {
 				}
 			}
 		}
-
 	}
 
-	private void createListTaxis() {
-		int startNumbersTaxi = r.nextInt(60) + 5;
-
+	private void createTaxis(int x, int y) {
+		int startNumbersTaxi = rand.nextInt(60) + 5;
 		for (int i = 0; i < startNumbersTaxi; i++) {
-			taxis.add(addTaxi());
+			taxis.add(addTaxi(x, y));
 		}
 	}
 
-	@Override
-	public Taxi addTaxi() {
-		int x = r.nextInt(10000 + valueX) - 5000;
-		int y = r.nextInt(10000 + valueY) - 5000;
-
-		Taxi newTaxi = new Taxi(x, y);
-
-		return newTaxi;
+	private Taxi addTaxi(int x, int y) {
+		int xRand = rand.nextInt(10000 + x) - 5000;
+		int yRand = rand.nextInt(10000 + y) - 5000;
+		return new Taxi(xRand, yRand);
 	}
 
-	@Override
-	public void removeTaxi(int index) {
+	private void removeTaxi(int index) {
 		taxis.remove(index);
 	}
 
-	@Override
-	public void updateListTaxis() {
+	private void updateTaxis() {
 		for (Taxi taxi : taxis) {
-			int shift = r.nextInt(14);
-			int choice = r.nextInt(4);
+			int shift = rand.nextInt(14);
+			int choice = rand.nextInt(4);
 			int changeValue = 0;
 
 			switch (choice) {
 			case 0:
-				changeValue = taxi.getX() - shift;
-				taxi.setX(changeValue);
+				changeValue = taxi.x - shift;
+				taxi.x = changeValue;
 				break;
 			case 1:
-				changeValue = taxi.getX() + shift;
-				taxi.setX(changeValue);
+				changeValue = taxi.x + shift;
+				taxi.x = changeValue;
 				break;
 			case 2:
-				changeValue = taxi.getY() - shift;
-				taxi.setY(changeValue);
+				changeValue = taxi.y - shift;
+				taxi.y = changeValue;
 				break;
 			case 3:
-				changeValue = taxi.getY() + shift;
-				taxi.setY(changeValue);
-				break;
-			default:
+				changeValue = taxi.y + shift;
+				taxi.y = changeValue;
 				break;
 			}
 		}
 	}
 
-	private void removeTaxisFromList() {
+	private void removeTaxis() {
 		if (taxis.size() > 7) {
-			int numbersRemoveTaxis = r.nextInt(2) + 1;
-			for (int i = 0; i < numbersRemoveTaxis; i++) {
-				int indexRemove = r.nextInt(taxis.size());
-				removeTaxi(indexRemove);
+			int numbersOfTaxiToRemove = rand.nextInt(2) + 1;
+			for (int i = 0; i < numbersOfTaxiToRemove; i++) {
+				int indexToRemove = rand.nextInt(taxis.size());
+				removeTaxi(indexToRemove);
 			}
 		}
 	}
 
-	private void addTaxisToList() {
+	private void addTaxis(int x, int y) {
 		if (taxis.size() < 62) {
-			int numbersAddTaxis = r.nextInt(2) + 1;
+			int numbersAddTaxis = rand.nextInt(2) + 1;
 			for (int i = 0; i < numbersAddTaxis; i++) {
-				taxis.add(addTaxi());
+				taxis.add(addTaxi(x, y));
 			}
 		}
 	}
 
-	private void checkNearestTaxi() {
+	private Taxi findNearestTaxi(int x, int y) {
+		Taxi nearestTaxi = taxis.get(0);
 
-		nearestTaxi = taxis.get(0);
 		for (int i = 1; i < taxis.size(); i++) {
-			int absoutValueDifferentCheckTaxi = Math.abs((taxis.get(i).getX() - valueX)) + Math.abs((taxis.get(i).getY() - valueY));
-			int absoutValueDifferentActualTaxi = Math.abs((nearestTaxi.getX() - valueX)) + Math.abs((nearestTaxi.getY() - valueY));
+			int absoutValueDifferentCheckTaxi = Math.abs((taxis.get(i).x - x)) + Math.abs((taxis.get(i).y - y));
+			int absoutValueDifferentActualTaxi = Math.abs((nearestTaxi.x - x)) + Math.abs((nearestTaxi.y - y));
 
 			if (absoutValueDifferentCheckTaxi < absoutValueDifferentActualTaxi) {
 				nearestTaxi = taxis.get(i);
-
 			}
 		}
+
+		return nearestTaxi;
 	}
 
 }
